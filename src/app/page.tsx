aaa3a1ef3e8ca01d9c8c053799bc589c0a26a74e
RojkovYury <rojkov.yury@gmail.com>
2023-10-27 13:44:25 +0300

    Telegram.WebApp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { useCallback, useEffect, useState } from 'react';
 import { Box, Paper } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import PersonIcon from '@mui/icons-material/Person';
@@ -7,10 +7,15 @@ import CreditCardIcon from '@mui/icons-material/CreditCard';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import TelegramInput from '@/components/telegram-input';
 
+const tg = window.Telegram.WebApp;
+tg.MainButton.isVisible = true;
+tg.MainButton.text = 'Отправить данные';
+tg.headerColor = 'secondary_bg_color';
+
 export default function Home() {
   return (
     <main>
-      use client
+      tg = window.Telegram.WebApp
       <Paper elevation={3} sx={{ borderRadius: '25px', backgroundColor: 'var(--tg-theme-bg-color)', display: 'flex', flexDirection: 'column', mx: 3, my: 8, px: 2, py: 2 }}>
         <TelegramInput
           id='card-number'
