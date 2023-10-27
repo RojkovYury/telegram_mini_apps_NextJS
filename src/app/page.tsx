0f23927dc9257c2a9b38ce4931f34e5ba3a8555a
RojkovYury <rojkov.yury@gmail.com>
2023-10-27 13:37:13 +0300

    use client 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Box, Paper } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import PersonIcon from '@mui/icons-material/Person';
@@ -8,6 +10,7 @@ import TelegramInput from '@/components/telegram-input';
 export default function Home() {
   return (
     <main>
+      use client
       <Paper elevation={3} sx={{ borderRadius: '25px', backgroundColor: 'var(--tg-theme-bg-color)', display: 'flex', flexDirection: 'column', mx: 3, my: 8, px: 2, py: 2 }}>
         <TelegramInput
           id='card-number'
