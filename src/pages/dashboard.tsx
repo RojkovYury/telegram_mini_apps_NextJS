9c5d7330c1376e829bfbe68c69ca26caa96930b7
RojkovYury <rojkov.yury@gmail.com>
2023-10-28 04:24:33 +0300

    lint

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,16 +5,19 @@ import { useGlobalContext } from "context/GlobalContext";
 
 const icons = [
   <path
+    key={1}
     strokeLinecap="round"
     strokeLinejoin="round"
     d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
   />,
   <path
+    key={2}
     strokeLinecap="round"
     strokeLinejoin="round"
     d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
   />,
   <path
+    key={3}
     strokeLinecap="round"
     strokeLinejoin="round"
     d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   if (typeof window !== "undefined") {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter();
     router.replace("/dashboard");
   }
