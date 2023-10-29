cd62173ab92db55f89cbc18c6da70c4300929526
RojkovYury <rojkov.yury@gmail.com>
2023-10-29 04:40:54 +0300

    empty_next_init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Roboto_Flex } from 'next/font/google';
+import './globals.css'
+
+const font = Roboto_Flex({ subsets: ['latin'], fallback: ['Helvetica Neue'] });
+
+export const metadata: Metadata = {
+  title: 'Card Form',
+  description: 'telegram_mini_apps_NextJS',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
