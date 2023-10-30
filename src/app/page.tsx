06569e6120468efaf87a917ec6e0b464e13f049a
RojkovYury <rojkov.yury@gmail.com>
2023-10-30 13:34:17 +0300

    ct2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Script from 'next/script';
+import Test from './components/test';
 
 if (typeof window !== 'undefined') {
   const tg = window.Telegram.WebApp;
   tg.MainButton.isVisible = true;
   tg.MainButton.text = 'Отправить данные';
-  tg.headerColor = 'secondary_bg_color';
+  // tg.headerColor = 'secondary_bg_color';
 }
-
 
 export default function Home() {
   return (
@@ -17,6 +17,7 @@ export default function Home() {
         strategy="beforeInteractive"
       />
       <main>
+        <Test/>
         <div style={{ color: 'var(--tg-theme-button-color)', background: 'var(--tg-theme-text-color)' }}>
           test
         </div>
